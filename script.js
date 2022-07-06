a736231bb5bdffdeee1af8f2b52a61b4ee0dc405
LSton40 <logan.sutton@gmail.com>
2022-07-06 16:50:02 -0500

    All code but getItem stick

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 var day = moment().format('dddd');
 var date = moment().format('MMMM Do YYYY, h:mm:ss a');
 var timeNow = moment().format('LT');
-// var dateTemp = new Date();
-// var hour = dateTemp.getHours();
-
 var hour = moment().hour();
-
-
-// console.log(hour2);
 
 var todaysDate = document.querySelector('#currentDay');
 var hourBlock = document.querySelectorAll('.hour');
 var textTime = document.querySelectorAll('.description');
 
-var saveBtn = document.querySelectorAll('.saveBtn');
-
-// var hourVal = hourBlock.id;
-
-console.log(todaysDate.id);
-
 var timeDay = document.querySelectorAll('.time-block');
+
+var saveBtn = document.querySelectorAll('.saveBtn');
 
 todaysDate.innerText = day + ', ' + date;
 
@@ -29,57 +19,92 @@ for (i=0; i < hourBlock.length; i++) {
     var hourVal = hourBlock[i].id;
     if (hourVal > hour) {
         textTime[i].classList.add('future');
-        // textTime[i].classList.remove('past');
-        // textTime[i].classList.remove('present');
     } 
     else if (hourVal == hour) {
         textTime[i].classList.add('present');
-        // textTime[i].classList.remove('past');
-        // textTime[i].classList.remove('future');
     } else {
         textTime[i].classList.add('past');
-        // textTime[i].classList.remove('present');
-        // textTime[i].classList.remove('future');
     }
 
 };
 
 
-// var userBox = document.querySelector('#hank');
-// var btn = document.querySelector('#larry');
+// function vanilly() {
+for (i=0; i < textTime.length; i++) {
 
-// btn.addEventListener('click', function (event) {
-//     event.preventDefault();
-    
-//     localStorage.setItem('wombat', userBox.value);
-  
+    // textTime[i].value = vanilly();
+    // var key = i;
 
-// });
+    // localStorage.getItem(key)
+    // if (key !== null) {
+        // var textBox = textTime[i].value;
+        // textBox = localStorage.getItem(key);
+    // }
+
+    saveBtn[i].addEventListener('click', function(event) {
+        event.preventDefault();
+        
+
+        
+        var parentEl = event.target.parentElement;
+        var textBoxValue = parentEl.querySelector('.description').value;
+
+        var key = parentEl.querySelector('.description').id;
+        console.log(key);
+
+
+        localStorage.setItem(key, textBoxValue);
+        
+        // var storage = localStorage.getItem(key);
+        
+        // textBoxValue = localStorage.getItem(key);
+        // console.log(textBox)
+        // return textBox;
+
+        
+        return key;
+        // return storage;
+    });
+
+    // var callUp = function() {
+    //     localStorage.getItem(key);
+    // };
+
+    // textBoxValue = callUp();
+    // if (i === key) {
+    //     textBoxValue = localStorage.getItem(key);
+    // }
+
+    // if (i == key) {
+
+    //     console.log(textTime[i]);
+    //     textTime[i].value = localStorage.getItem(key);
+    // }
+};
+// }
 
 for (i=0; i < textTime.length; i++) {
-    
-    
-    var key = 'agenda' + i;
 
-    // textTime[i].value = localStorage.getItem(key);
+    // function assignVal(event) {
+        var key;
+        localStorage.getItem(key);
+
+        if (textTime[i].id == key)
+    // var parentEl = event.target.parentElement;
+    // var textBoxValue = parentEl.querySelector('.description').value;
+    // var key = parentEl.querySelector('.description').id;
 
     
+    textTime[i].value = localStorage.getItem(key);
     
-
-    // console.log(key);
-
-    saveBtn[i].addEventListener('click', function (event) {
-    event.preventDefault();
-    var textBox = textTime[i].value;
-    for (i=0; i < saveBtn.length; i++) {
-        
-    localStorage.setItem(key, textBox);
-    
-}
-    
-    // localStorage.getItem(key);
-    // console.log(textBox)
-    // return textBox;
-});
-
+// }
 };
+// for (i=0; i < textTime.length; i++) {
+
+//     if (textTime[i] == key) {
+//         textTime[i].value = localStorage.getItem(key);
+//     }
+// };
+
+
+
